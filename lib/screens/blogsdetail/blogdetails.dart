08970e3251b7325f44b536848eb6a6fd1768d5ef
Prajkta Gherade <prajktagherade@gmail.com>
2023-09-18 11:16:21 +0530

    department detail page done

--- lib/screens/blogsdetail/blogdetails.dart
+++ lib/screens/blogsdetail/blogdetails.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
-
 import '../components/footer/footer.dart';
 import '../components/header.dart';
 import '../newsandupdates/newsandupdates.dart';
